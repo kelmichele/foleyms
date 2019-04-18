262c39985c28db565a4a9dcc72513eff8169b36c
kelmichele <kelli@hsdesignhouse.com>
2019-04-18 11:41:49 -0500

    available property page

--- src/hoc/Pages/Listings/Listings.js
+++ src/hoc/Pages/Listings/Listings.js
@@ -14,21 +14,15 @@ import Listing3 from "../../../wLaurel200.pdf";
 const listings = () => {
   return (
     <div className={classes.listingsPage}>
-
-      <PageHeader phImage={phBG} ovPacity=".5" hColor="white" pageTitle="Available Property" />
-
-      {/* <p>Just eleven miles north of Alabama’s white sand beaches, Foley’s historic downtown epitomizes old Alabama charm.  Historic sites, boutique retailers and unique restaurants line the walkways of this quaint Main Street making visitors feel like they’ve stepped back in time.  </p>
-      <p>Among its traditional architecture and old-world style, downtown Foley features restaurants, gift shops, barbershops, antique markets and even a hidden spa for unwinding after a long day of shopping.  With beautiful flowers dotting intersections, a streetlight lined seven-acre park, and the colorful hanging floral baskets, Foley captivates visitors and residents alike with its picturesque beauty.   The newly remodeled vacant buildings and downtown revitalization add to this area’s appeal thus attracting a plethora of new, diverse businesses and potential downtown housing.  Professional offices are sprinkled among the Main Street district offering conveniences to the nearby residents.  It has also become a popular lunch destination due to its eclectic cuisine options including Mediterranean, Italian, vegan, and Southern-style restaurants all within a stone’s throw of one another. Quaint coffee shops and local art shops can also be found in this charming area.</p>
-      <p>Brimming with history, downtown Foley is home to two city museums housed in their original, historic buildings and an “old time” soda fountain drugstore that has donned the same corner since 1929.  The Holmes Medical Museum is housed in Baldwin County’s first hospital and showcases extensive displays of medical equipment and patient rooms as they would have been in the 1930s and ’40s. The Foley Railroad Museum includes a model train exhibit, antique train cars and train rides around the park. It is housed in the Depot which was built in 1909 and has been the hub of activity in the City for over 60 years. </p>
-      <p>Downtown Foley is also home to the Wilbourne Antique Rose Trail which follows the route of the Louisville and Nashville Railroad into downtown Foley. You can walk the landscaped trail while enjoying the beauty and fragrance of hundreds of roses displayed in a variety of sizes and color. </p>
-      <p>Located at the heart of Baldwin County’s busiest intersection with an average of forty-thousand cars per day, Foley has significant commercial appeal, especially with its newly built pedestrian bridge connecting the shops of downtown with the park and city buildings allowing accessibility and walkability for visitors and residents alike.</p> */}
+      <PageHeader phImage={phBG} ovPacity=".2" hColor="white" pageTitle="Available Property" />
 
       <div className={classes.intro}>
         <div className={classes.insideNrw}>
-          <p>Located at the heart of Baldwin County’s busiest intersection, with an average of forty-thousand cars per day, Foley has 
-            significant commercial appeal, especially with its newly built pedestrian bridge connecting the shops of downtown with the 
-            park and city buildings allowing accessibility and walkability.
-          </p>
+          <p>With all of the history and beautiful scenery that Foley has to offer, on top of its prime location and high foot traffic, 
+          it is no surprise that businesses of all kinds are attracted to the downtown area.
+          Take a look at the properties currently available in Foley.
+          Select a listing to learn more, and feel free to contact us with questions. </p>
+          {/* <p>Listings updated every 6 weeks.</p> */}
         </div>
       </div>
 
@@ -63,6 +57,7 @@ const listings = () => {
               </div>
             </a>
           </div>
+        
         </div>
       </div>
     </div>
