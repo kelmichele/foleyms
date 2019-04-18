523df422e32903b4e0765b5df195d814fcb0cffa
kelmichele <kelli@hsdesignhouse.com>
2019-04-18 14:02:48 -0500

    about 90% -- except images

--- src/hoc/Pages/About/About.js
+++ src/hoc/Pages/About/About.js
@@ -3,10 +3,14 @@ import classes from "./About.scss";
 import { Helmet } from "react-helmet";
 // import PageHeader from "../../../components/PageHeader/PageHeader";
 import Parallax from "../../../components/Parallax/Parallax";
-import phImage from "../../../assets/images/foley6BW.jpg";
-import bodyImg2 from "../../../assets/images/mm-appetizer2.jpg";
+import phImage from "../../../assets/images/TEMProses.jpg";
+// import bodyImg2 from "../../../assets/images/mm-appetizer2.jpg";
+import bodyImg2 from "../../../assets/images/TEMPfountain.jpg";
 import ImageText from "../../../components/ImageText/ImageText";
 import bodyImg3 from "../../../assets/images/foley6.jpg";
+import bodyImg4 from "../../../assets/images/TEMPplaza.jpg";
+import ImgRow1 from "../../../assets/images/modelTrain1.jpg";
+import ImgRow2 from "../../../assets/images/railroad1.jpg";
 import Aux from "../../../hoc/Aux/Aux";
 // import Pdf from "../../../foley-walking-map.pdf";
 
@@ -20,12 +24,6 @@ const textContent0 = (
     <p>Among its traditional architecture and old-world style, downtown Foley features restaurants, gift shops, barbershops, antique markets and even a 
     hidden spa for unwinding after a long day of shopping. With beautiful flowers dotting intersections, a streetlight lined seven-acre park, and the 
     colorful, hanging floral baskets, Foley captivates visitors and residents alike with its picturesque beauty.</p> 
-
-    <p>The newly remodeled vacant buildings and downtown revitalization add to this area’s appeal, thus attracting a plethora of new, 
-    diverse businesses and potential downtown housing. Professional offices are sprinkled among the Main Street district,
-    offering conveniences to the nearby residents. It has also become a popular lunch destination due to its eclectic cuisine options,
-    including Mediterranean, Italian, vegan, and Southern-style restaurants all within a stone’s throw of one another. 
-    Quaint coffee shops and local art shops can also be found in this charming area.</p>
   </div>
 );
 
@@ -35,7 +33,7 @@ const textContent1 = (
       You can walk the landscaped trail while enjoying the beauty and fragrance of hundreds of roses displayed in a variety of sizes and color. 
     </p>
     
-    <p>Located at the heart of Baldwin County’s busiest intersection, with an average of forty-thousand cars per day, Foley has significant commercial appeal, 
+    <p className={classes.ShadowFloat}>Located at the heart of Baldwin County’s busiest intersection, with an average of forty-thousand cars per day, Foley has significant commercial appeal, 
       especially with its newly built pedestrian bridge connecting the shops of downtown with the park and city buildings allowing accessibility and walkability 
       for visitors and residents alike.
     </p>
@@ -53,20 +51,47 @@ const about = () => {
 
 
     <div className={classes.aboutPage}>
-      
-      <Parallax plaxStyle="styleHd" plaxHeadline={pH1} plaxImage={phImage} />
+      <Parallax plaxStyle="styleHdLg" plaxHeadline={pH1} plaxImage={phImage} />
 
-      <div className={classes.medDef}>
+      <div className={classes.insideNrw}>
         <div className={classes.intro}>
           <p className={classes.tpadding4}>Just eleven miles north of Alabama’s white sand beaches, Foley’s historic downtown epitomizes old Alabama charm.  
             Historic sites, boutique retailers and unique restaurants line the walkways of this quaint Main Street, making visitors feel like they’ve stepped back in time.
           </p>
         </div>
-        
-        <div className={classes.abBody}>
-          <ImageText textContent={textContent0} image={bodyImg2} addPadd="xsm" gridType="textBig" />
-            
-          <div className={classes.borderRow}>
+
+      </div>
+
+      <div className={classes.abBody}>
+        <div className={classes.med2Def}>
+          <ImageText textContent={textContent0} image={bodyImg2} addPadd="xsm" /> 
+        </div>
+
+        <div className={[classes.relTitle, classes.BgAlt].join(' ')}>
+          <img src={bodyImg4} className={[classes.imgResponsive, classes.imgCenter].join(' ')} alt="Foley" />
+          <div className={classes.insideNrw}>
+            <div className={classes.TextBox}>
+              <p>The newly remodeled vacant buildings and downtown revitalization add to this area’s appeal, thus attracting a plethora of new,
+              diverse businesses and potential downtown housing. Professional offices are sprinkled among the Main Street district,
+              offering conveniences to the nearby residents. It has also become a popular lunch destination due to its eclectic cuisine options,
+              including Mediterranean, Italian, vegan, and Southern-style restaurants all within a stone’s throw of one another.
+              Quaint coffee shops and local art shops can also be found in this charming area.</p>
+            </div>
+          </div>
+        </div>
+
+
+        <div className={classes.ImgRow}>
+          <div className={classes.inside}>
+            <img src={ImgRow1} className={[classes.imgResponsive, classes.ImgL].join(' ')} alt="Foley" />
+            <img src={ImgRow2} className={[classes.imgResponsive, classes.ImgR].join(' ')} alt="Foley" />
+          </div>
+        </div>
+
+
+
+        <div className={classes.medDef}>            
+          {/* <div className={classes.borderRow}>
             <div className={classes.insideXl}>
               <p>Brimming with history, downtown Foley is home to two city museums housed in their original, historic buildings, and an “old time” soda fountain drugstore 
                 that has donned the same corner since 1929. The Holmes Medical Museum is housed in Baldwin County’s first hospital, and showcases extensive displays of
@@ -74,7 +99,7 @@ const about = () => {
                 cars, and train rides around the park. It is housed in the Depot, which was built in 1909, and has been the hub of activity in the City for over 60 years. 
               </p>
             </div>
-          </div>
+          </div> */}
 
           <ImageText textContent={textContent1} image={bodyImg3} addPadd="xsm" textFloat="textRight" />
         </div>
