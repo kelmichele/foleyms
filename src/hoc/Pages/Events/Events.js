016644e3deb6d78d21da3a793db68a0080bc82bf
kelmichele <kelli@hsdesignhouse.com>
2019-05-19 11:55:48 -0500

    events page

--- src/hoc/Pages/Events/Events.js
+++ src/hoc/Pages/Events/Events.js
@@ -1,8 +1,9 @@
 import React from "react";
 import classes from "./Events.scss";
 import { Helmet } from "react-helmet";
-import PageHeader from "../../../components/PageHeader/PageHeader";
-import phImage from "../../../assets/images/Foley-30Head.jpg";
+// import PageHeader from "../../../components/PageHeader/PageHeader";
+// import phImage from "../../../assets/images/Foley-30Head.jpg";
+import phBG from "../../../assets/images/Foley-3046.jpg";
 
 class Events extends React.Component {
   render() {
@@ -12,8 +13,18 @@ class Events extends React.Component {
           <title>Events Around Foley, Al</title>
           <meta name="description" content="" />
         </Helmet>
-        <PageHeader phImage={phImage} ovPacity=".15" hColor="white" pageTitle="Upcoming Events" />
+        {/* <PageHeader phImage={phImage} ovPacity=".15" hColor="white" pageTitle="Upcoming Events" /> */}
 
+        <div className={classes.EventsHead}>
+          <div className={classes.med2Def}>
+            <img src={phBG} alt="real estate item" className={classes.imgRel} />
+
+            <div className={classes.intText}>
+              <h1>Upcoming Events</h1>
+              <p>Don't miss out on all the fun things happening in the Foley area. Select an event from the calendar below for more information. </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
